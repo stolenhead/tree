e082b37c7b609a3d4fce5af4d1fa5be899c5f84a
stolenhead <andrea.palomino.ruby@gmail.com>
2017-07-03 17:57:20 -0500

    mejora function mostrar tipo

--- public/main2.js
+++ public/main2.js
@@ -2,9 +2,9 @@ var template = '<div id="num_gestas" class="inline">{{numgestas}}</div>'+
 				'<svg height="10" width="100"><line x1="0" y1="10" x2="100" y2="10" style="stroke:rgb(255,0,0);stroke-width:2" /></svg>';
  var template2 ='<div id="num_bebes" class="inline">{{numbebes}}</div>'+
     	        '<svg height="10" width="100"><line x1="0" y1="10" x2="100" y2="10" style="stroke:rgb(255,0,0);stroke-width:2" /></svg>';
- var template3 ='<div id="num_nacidos" class="inline">{{numnacidos}}</div>'+
+ var template3 ='<div id="num_tipo" class="inline">{{numtipo}}</div>'+
     	        '<svg height="10" width="100"><line x1="0" y1="10" x2="100" y2="10" style="stroke:rgb(255,0,0);stroke-width:2" /></svg>';
-var template4= '<div id="num_muertos" class="inline">{{nummuertos}}</div>'+
+var template4= '<div id="num_vivos" class="inline">{{nummuertos}}</div>'+
     	        '<svg height="10" width="100"><line x1="0" y1="10" x2="100" y2="10" style="stroke:rgb(255,0,0);stroke-width:2" /></svg>';
     	
 var mostrar=function(){
@@ -13,17 +13,22 @@ var mostrar=function(){
     url: window.location.href + "data_2.json",
     type:'GET',
     success: function(response){
-      
       	var data=response.gestantes[0];
       	var mostrar_numges ="";
       	var mostrar_numbebes="";
+      	var mostrar_tipos="";
       	mostrar_numges=template.replace("{{numgestas}}",data.num_gestas);
       	mostrar_numbebes=template2.replace("{{numbebes}}",data.fetos.length);
-      	// mostrar_numbebes=template.replace("{{numbebes}}",data.fetos.length);
-      	$("#usuario").append(mostrar_numges,mostrar_numbebes);
+      	$("#usuario").append(mostrar_numges,mostrar_numbebes,mostrar_tipos);
+      	var fetos =data.fetos.length;
+      	var feto=data.fetos;
+      	for(var i=0;fetos>i;i++){
+      		var terminacio =feto[i].terminacion;
+      		mostrar_tipos=template3.replace("{{numtipo}}",terminacion.typeof(vaginal).length);
+      	}
       	mostrar_numges="";
 		    mostrar_numbebes="";
-	  
+	      mostrar_tipos="";
     },
     error:function(error){
       console.log(error);
